cedce76406be987cafe3a3a1094e026b6ddc8a05
RafaelAlvesoliveira <faelalvesoliveira06@gmail.com>
2022-04-12 18:42:09 -0300

    Correção do módulo 7, erro de versionamento não identificado

---
+++ 7_TEMPLATE_ENGINE_EXPRESS/5_else/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard')
+})
+
+app.get('/', (req, res) => {
+  const user = {
+    name: "Rafael",
+    surname: "Alves",
+    age: 31
+  }
+  const palavra = 'Teste'
+
+  const auth = true
+
+  const approved = true
+
+  res.render('home', { user: user, palavra, auth })
+})
+
+app.listen(3000, () => {
+  console.log('App funcionado!')
+})
---
+++ 7_TEMPLATE_ENGINE_EXPRESS/6_loops/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+
+  const items = ["Item a", "Item b", "Item c"]
+
+  res.render('dashboard', {items})
+})
+
+app.get('/', (req, res) => {
+  const user = {
+    name: "Rafael",
+    surname: "Alves",
+    age: 31
+  }
+  const palavra = 'Teste'
+
+  const auth = true
+
+  const approved = true
+
+  res.render('home', { user: user, palavra, auth })
+})
+
+app.listen(3000, () => {
+  console.log('App funcionado!')
+})
