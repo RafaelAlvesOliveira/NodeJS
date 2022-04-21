8d4034e2bc15c592d3c2610a89cbc02627029a50
RafaelAlvesoliveira <faelalvesoliveira06@gmail.com>
2022-04-20 23:36:53 -0300

    Inclusão de arquivos com recuperação de informações salvas no banco, edição, atualização, relacionamento entre outras coisas.

---
+++ 9_SEQUELIZE/11_select_relacionado/models/Address.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize')
+
+const db = require('../db/conn')
+
+const User = require('./User')
+
+const Address = db.define('Address', {
+
+  street: {
+    type: DataTypes.STRING,
+    allowNull: false,    
+  },
+  number: {
+    type: DataTypes.STRING,    
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,    
+  }
+})
+
+Address.belongsTo(User)
+
+module.exports = Address
